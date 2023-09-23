48f01394ed9bcdcc9ba79fa419808093090a957f
devil_1nc <diamondoriginal367@gmail.com>
2023-09-23 20:21:21 +0400

    ну типа

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ app.config['UPLOAD_FOLDER'] = "/upload"
 def index():
     return render_template('index.html')
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save("upload/" + f.filename)
-      return 'file uploaded successfully'
 
-if __name__=="__main__":
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save("upload/" + f.filename)
+        return 'file uploaded successfully'
+
+
+if __name__ == "__main__":
     app.run(debug=True)
